1cf79624469e633f0421cbc8f9d25401d7193ac2
TryExceptElse <schwarzjohnv@gmail.com>
2020-10-04 01:01:49 -0700

    Add entry point to setup.py.

--- setup.py
+++ setup.py
@@ -5,4 +5,8 @@ setup(
     name='copywriter',
     tests_require=['pytest'],
     python_requires='>=3.6',
+    py_modules=['copywriter'],
+    entry_points={
+        'console_scripts': ['copywriter=copywriter:main'],
+    }
 )
